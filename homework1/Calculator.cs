d1599a4807cf4eeb43592fc2a38346bf651808df
kasia-cz <czak.katarzyna1@gmail.com>
2023-12-02 23:14:05 +0100

    Infinite loop added

--- homework1/Calculator.cs
+++ homework1/Calculator.cs
@@ -4,44 +4,83 @@
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Welcome to my fantastic calculator!");
-            Console.WriteLine("Select the operation you want to perform by entering the number:");
-            Console.WriteLine("1 - addition");
-            Console.WriteLine("2 - subtraction");
-            Console.WriteLine("3 - multiplication");
-            Console.WriteLine("4 - division");
-            Console.WriteLine("5 - exponentiation");
-            Console.WriteLine("6 - factorial");
-            var operation = int.Parse(Console.ReadLine()); // TO DO: try parse x, y, operation
 
-            Console.WriteLine("Enter the first number below:");
-            var x = int.Parse(Console.ReadLine());
-
-            var y = 0;
-
-            if (operation != 6)
+            bool runCalculator = true;
+            while (runCalculator)
             {
-                Console.WriteLine("Enter the second number below:");
-                y = int.Parse(Console.ReadLine());
+                Console.WriteLine("Select the operation you want to perform by entering the number:");
+                Console.WriteLine("1 - addition");
+                Console.WriteLine("2 - subtraction");
+                Console.WriteLine("3 - multiplication");
+                Console.WriteLine("4 - division");
+                Console.WriteLine("5 - exponentiation");
+                Console.WriteLine("6 - factorial");
+                Console.WriteLine("Or type 'exit' to end the program.");
+
+                var operationInput = Console.ReadLine();
+
+                if (operationInput.Equals("exit", StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+
+                if (!int.TryParse(operationInput, out int operation) || operation < 1 || operation > 6)
+                {
+                    Console.WriteLine("Invalid operation\n");
+                    continue;
+                }
+
+                if (operation == 6)
+                {
+                    Console.WriteLine("Enter the number below:");
+                }
+                else
+                {
+                    Console.WriteLine("Enter the first number below:");
+                }
+
+                // TO DO: try parse x, y
+
+                var x = int.Parse(Console.ReadLine());
+
+                var y = 0;
+
+                if (operation != 6)
+                {
+                    Console.WriteLine("Enter the second number below:");
+                    y = int.Parse(Console.ReadLine());
+                }
+
+                if (operation == 4 && y == 0)
+                {
+                    Console.WriteLine("You cannot divide by zero!\n");
+                    continue;
+                }
+
+                double result = operation switch
+                {
+                    1 => Add(x, y),
+                    2 => Subtract(x, y),
+                    3 => Multiply(x, y),
+                    4 => Divide(x, y),
+                    5 => Power(x, y),
+                    6 => Factorial(x)
+                };
+
+                Console.WriteLine($"Your result: {result}");
+
+                Console.WriteLine("Type 'exit' to end the program or press enter to continue.");
+                var exitInput = Console.ReadLine();
+                runCalculator = !exitInput.Equals("exit", StringComparison.OrdinalIgnoreCase);
             }
-
-            double result = operation switch
-            {
-                1 => Add(x, y),
-                2 => Subtract(x, y),
-                3 => Multiply(x, y),
-                4 => Divide(x, y),
-                5 => Power(x, y),
-                6 => Factorial(x)
-            };
-
-            Console.WriteLine($"Your result: {result}");
-
+            Console.WriteLine("Program ended. Goodbye!");
         }
         private static int Add(int x, int y) { return x + y; }
         private static int Subtract(int x, int y) { return x - y; }
         private static int Multiply(int x, int y) { return x * y; }
-        private static double Divide(int x, int y) { return x / y; } // TO DO: division by 0 error
+        private static double Divide(int x, int y) { return x / y; }
         private static double Power(int x, int y) { return Math.Pow(x, y); }
         private static int Factorial(int x)
         {
